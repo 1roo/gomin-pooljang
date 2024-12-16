d29e23a482bf925f9fbc1f23aaf7704608986e08
1roo <93hanaru@gmail.com>
2024-12-16 14:12:11 +0900

    build: 기본 폴더 구성성

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const db = require("./models");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use("/static", express.static(__dirname + "/static"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const indexRouter = require("./routes");
+app.use("/", indexRouter);
+
+app.get("*", (req, res) => {
+  res.render("404");
+});
+
+db.sequelize.sync({ force: false }).then((result) => {
+  console.log("DB연결 성공");
+  console.log("------------------------------");
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+  });
+});
---
+++ models/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const Sequelize = require("sequelize");
+
+let config = require(__dirname + "/../config/config.js");
+console.log("config", config);
+config = config["development"];
+
+const db = {};
+
+let sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = require("./User")(sequelize, Sequelize);
+db.Message = require("./Message")(sequelize, Sequelize);
+
+module.exports = db;
