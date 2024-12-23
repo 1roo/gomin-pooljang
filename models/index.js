c11c9cbb13f1e3d8424fa4ff2d4fcf9e4845a8b1
1roo <93hanaru@gmail.com>
2024-12-23 12:09:27 +0900

    etc/241223/배포를 위한 수정정

--- models/index.js
+++ models/index.js
@@ -2,9 +2,8 @@
 
 const Sequelize = require("sequelize");
 
-let config = require(__dirname + "/../config/config.js");
-console.log("config", config);
-config = config["development"];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
 
 const db = {};
 
