409baef366d917d9f3920c149c30a3ee52082b85
1roo <93hanaru@gmail.com>
2024-12-27 15:24:45 +0900

    feat/241227/userController 비밀번호 재설정

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const db = require("./models");
 const app = express();
 require("dotenv").config();
-const UserController = require("./controller/CUser");
 const PORT = process.env.PORT;
 const cors = require("cors");
 
@@ -18,61 +17,6 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 app.use("/", indexRouter);
 app.use(cors());
-
-app.get("/mypage/user_received_msg", (req, res) => {
-  res.render("user_received_msg");
-});
-
-app.get("/mypage/user_sended_msg", (req, res) => {
-  res.render("user_sended_msg");
-});
-/**
- * 이메일 중복여부
- * 작성자: 하나래
- */
-app.post("/check-email", UserController.checkEmail);
-
-/**
- * 회원가입
- * 작성자: 하나래
- */
-app.post("/regist", UserController.registUser);
-
-/**
- * 로그인
- * 작성자: 하나래
- */
-app.post("/login", UserController.loginUser);
-
-/**
- * 토큰 검증
- * 작성자: 하나래
- */
-app.post("/token", UserController.validation);
-
-/**
- * 비밀번호 수정
- * 작성자: 하나래
- */
-app.patch("/changePw", UserController.changePw);
-
-/**
- * 로그아웃
- * 작성자: 하나래
- */
-app.post("/logout", UserController.logout);
-
-/**
- * 회원탈퇴
- * 작성자: 하나래
- */
-app.post("/deleteAccount", UserController.deleteAccount);
-
-/**
- * 회원가입질문 일치 여부
- * 작성자: 하나래
- */
-app.post("/findAccount", UserController.findAccount);
 
 app.get("*", (req, res) => {
   res.render("404");
